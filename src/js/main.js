ad731f2de2d8c87a759fc1150ef1f8ecf32a6f3d
vadymchepay <vadymbychkov@gmail.com>
2018-05-15 23:51:09 +0300

    next few sections

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,19 @@
         fullWidth: true,
         indicators: true
     });
+      
+    var testCarous = $('.testimonals_carousel .testimonals_carousel_container .carousel');
+    
+      testCarous.carousel({
+          fullWidth: true
+      });
+      
+      $('.control_prev').click(function(){
+          testCarous.carousel('prev')
+      });
+      $('.control_next').click(function(){
+          testCarous.carousel('next')
+      });
   });
 
 
